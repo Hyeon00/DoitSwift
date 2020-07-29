cf9c5d8bc2d65bca1242d78caf3ff266d3542362
Hyeon00 <sunghyun7949@naver.com>
2020-07-30 00:16:04 +0900

    Complete

--- PinchGesture/ViewController.swift
+++ PinchGesture/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var imgPinch: UIImageView!
+    
+    // 핀치 제스처가 발생했을 때 현재 글자 크기 저장.
+    var initialFontSize: CGFloat!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(ViewController.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
     }
-
-
+    
+    
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        // pinch 제스처의 상태를 state 속성을 사용하여 확인.
+        /*
+        if pinch.state == UIGestureRecognizer.State.began {
+            initialFontSize = txtPinch.font.pointSize
+        } else {
+            txtPinch.font = txtPinch.font.withSize(initialFontSize * pinch.scale)
+        }
+        */
+        
+        imgPinch.transform = imgPinch.transform.scaledBy(x: pinch.scale, y: pinch.scale)
+        pinch.scale = 1
+    }
+    
 }
 
